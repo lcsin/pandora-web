a1840819414db1572dc1ef5e199103eb8ab62ca2
lcsin <1847@qq.com>
2024-10-30 14:49:20 +0800

    注册登录/注册页面

--- src/router/index.js
+++ src/router/index.js
@@ -5,17 +5,17 @@ const router = createRouter({
   routes: [
     { path: '/', name: 'home', component: () => import('../views/Home.vue') },
     { path: '/index', name: 'index', component: () => import('../views/Home.vue') },
-    { path: '/login', name: 'login', component: () => import('../views/Login.vue') },
-    { path: '/register', name: 'register', component: () => import('../views/Register.vue') },
+    // { path: '/login', name: 'login', component: () => import('../views/Login.vue') },
+    // { path: '/register', name: 'register', component: () => import('../views/Register.vue') },
     { path: '/:pathMatch(.*)*', name: '404', component: () => import('../views/404.vue') },  // 访问不存在的路径时，直接跳转到404页面
   ]
 })
 
-router.beforeEach((to, from) => {
-  const token = localStorage.getItem("pandora.token")
-  if (token == null && to.name !== 'login' && to.name !== 'register') {
-    return { name: 'login' }
-  }
-})
+// router.beforeEach((to, from) => {
+//   const token = localStorage.getItem("pandora.token")
+//   if (token == null && to.name !== 'login' && to.name !== 'register') {
+//     return { name: 'login' }
+//   }
+// })
 
 export default router
--- src/utils/request.js
+++ src/utils/request.js
@@ -7,7 +7,7 @@ const request = axios.create({
 })
 
 request.interceptors.request.use(config => {
-    config.headers.Authorization = localStorage.getItem("pandora.token")
+    // config.headers.Authorization = localStorage.getItem("pandora.token")
     return config
 })
 
